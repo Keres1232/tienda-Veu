c9d665a6865c287616575eefbcc8e37cd0d5c91a
Keres1232 <162938616+Keres1232@users.noreply.github.com>
2026-02-20 13:28:14 -0500

    primera subida

---
+++ Tarea Vue/app.js
@@ -0,0 +1,85 @@
+const { createApp } = Vue;
+
+createApp({
+
+  data() {
+    return {
+      productos: [],
+      carrito: [],
+      busqueda: "",
+      modoOscuro: false
+    }
+  },
+
+  computed: {
+    productosFiltrados() {
+      return this.productos.filter(p =>
+        p.nombre.toLowerCase().includes(this.busqueda.toLowerCase())
+      );
+    },
+
+    total() {
+      return this.carrito.reduce((sum, item) => sum + item.precio, 0);
+    }
+  },
+
+  methods: {
+
+    async cargarProductos() {
+      const response = await fetch("productos.json");
+      this.productos = await response.json();
+    },
+
+    agregarAlCarrito(producto) {
+      if (producto.stock > 0) {
+        this.carrito.push(producto);
+        producto.stock--;
+      }
+    },
+
+    toggleModo() {
+      this.modoOscuro = !this.modoOscuro;
+    }
+
+  },
+
+  mounted() {
+    this.cargarProductos();
+  },
+
+  components: {
+  "producto-card": {
+    props: ["producto"],
+    template: `
+      <div 
+        class="card"
+        :class="{ agotado: producto.stock === 0 }"
+        :style="{ backgroundImage: 'url(' + producto.imagen + ')' }"
+      >
+
+        <div class="overlay">
+          <p class="text head">{{ producto.nombre }}</p>
+          <span>{{ producto.categoria }}</span>
+          <p class="text price">$ {{ producto.precio }}</p>
+
+          <p v-if="producto.stock > 0">
+            Stock: {{ producto.stock }}
+          </p>
+          <p v-else>Agotado</p>
+
+          <button 
+            @click="$emit('agregar', producto)"
+            :disabled="producto.stock === 0">
+            Agregar
+          </button>
+        </div>
+
+      </div>
+    `
+  }
+}
+
+
+
+
+}).mount("#app");
